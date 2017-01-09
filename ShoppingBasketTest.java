3a6e6ce1429d49238d16bc1c38cab7802f0f81be
chrisbacon <bacon.chris@gmail.com>
2017-01-09 11:00:12 +0000

    wrote test for adding items

--- ShoppingBasketTest.java
+++ ShoppingBasketTest.java
@@ -1,11 +1,21 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
+import org.mockito.*;
+import static org.mockito.Mockito.*;
+
 public class ShoppingBasketTest{
   ShoppingBasket basket;
+  Shoppable item;
 
   @Before 
   public void before() {
     basket = new ShoppingBasket ();
+    item = mock(Shoppable.class);
+  }
+
+  @Test
+  public void addTest() {
+    basket.add(item);
   }
 }
