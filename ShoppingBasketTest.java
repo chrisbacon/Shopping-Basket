a02d28771f1a2d6f3b04e04009adc306e38e9b7e
chrisbacon <bacon.chris@gmail.com>
2017-01-09 11:01:40 +0000

    Wrote test for getSize

--- ShoppingBasketTest.java
+++ ShoppingBasketTest.java
@@ -15,7 +15,18 @@ public class ShoppingBasketTest{
   }
 
   @Test
+  public void getSizeTest() {
+    assertEquals(0, basket.getSize());
+  }
+
+  @Test
   public void addTest() {
     basket.add(item);
+    assertEquals(1, basket.getSize());
+  }
+
+  @Test
+  public void removeTest() {
+
   }
 }
