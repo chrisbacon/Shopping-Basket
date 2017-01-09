8b2a50077edf631f1241fd3b696c18bf68b2786f
chrisbacon <bacon.chris@gmail.com>
2017-01-09 10:43:21 +0000

    initial commit

---
+++ ShoppingBasketTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ShoppingBasketTest{
+  ShoppingBasket basket;
+
+  @Before 
+  public void before() {
+    basket = new ShoppingBasket ();
+  }
+}
